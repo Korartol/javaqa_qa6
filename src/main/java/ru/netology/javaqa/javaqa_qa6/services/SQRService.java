e08c113e96fead621ab68793f7aedfbad83442db
Коробейников Артем <korartol@gmail.com>
2022-06-30 14:55:49 +0300

    Добавлена остановка цикла если диапазон до 100

--- src/main/java/ru/netology/javaqa/javaqa_qa6/services/SQRService.java
+++ src/main/java/ru/netology/javaqa/javaqa_qa6/services/SQRService.java
@@ -5,8 +5,12 @@ public class SQRService {
         int count = 0;
 
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= begin && i * i <= end) {
-                count++;
+            if (end >= 100) {  // если конечное значение меньше минимального корня, то прервать цикл.
+                if (i * i >= begin && i * i <= end) {
+                    count++;
+                }
+            } else {
+                break; // прерываем цикл, если i * i < 100
             }
         }
         return count;
